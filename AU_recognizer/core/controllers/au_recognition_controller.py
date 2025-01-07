9fc3926cb541191776bd8febfc1dfd54582d75d1
GiovanniPag <paganelli.giovanni.97@gmail.com>
2025-01-07 16:02:07 +0100

    controllers refactors part 2

---
+++ AU_recognizer/core/controllers/au_recognition_controller.py
@@ -0,0 +1,19 @@
+from AU_recognizer.core.controllers.base_controller import Controller
+from AU_recognizer.core.util import logger
+from AU_recognizer.core.views.view import AURecognitionView
+
+
+class AURecognitionController(Controller):
+    def __init__(self, master=None) -> None:
+        super().__init__()
+        self.view = None
+        self.master = master
+
+    def bind(self, v: AURecognitionView):
+        logger.debug(f"bind in AURecognition controller")
+        self.view = v
+        self.view.create_view()
+
+    def update_selected(self, data):
+        logger.debug(f"update selected in AURecognition controller")
+        self.view.update_selected_project(data)
