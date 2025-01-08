e70d6d6fc258539fda229eb649c52b6e2eed47b7
GiovanniPag <paganelli.giovanni.97@gmail.com>
2025-01-08 19:15:53 +0100

    rendering for tkinter frames effects with canvas and draw engine

---
+++ AU_recognizer/core/views/widgets/core_rendering/__init__.py
@@ -0,0 +1,12 @@
+import sys
+
+from .custom_canvas import CustomCanvas
+from .draw_engine import DrawEngine
+
+CustomCanvas.init_font_character_mapping()
+
+# determine draw method based on current platform
+if sys.platform == "darwin":
+    DrawEngine.preferred_drawing_method = "polygon_shapes"
+else:
+    DrawEngine.preferred_drawing_method = "font_shapes"
