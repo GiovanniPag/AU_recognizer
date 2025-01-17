80c61a155ebfa1a7afdf4fe1285ff64d0e5016b4
GiovanniPag <paganelli.giovanni.97@gmail.com>
2025-01-17 12:53:34 +0100

    refactored view file npy_viewer.py

---
+++ AU_recognizer/core/user_interface/views/npy_viewer.py
@@ -0,0 +1,25 @@
+import numpy as np
+import tkinter as tk
+
+from AU_recognizer.core.user_interface.views import View
+from AU_recognizer.core.user_interface.widgets.core_widget_classes import CustomTextbox
+
+
+class NpyViewer(View):
+    def __init__(self, master=None, path=None, **kwargs):
+        super().__init__(master, **kwargs)
+        self.master = master
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+        self.path = path
+
+    def create_view(self):
+        # Text widget for displaying the data
+        text_widget = CustomTextbox(self, wrap=tk.WORD)
+        text_widget.grid(column=0, row=0, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        data = np.load(self.path)
+        text_widget.delete("1.0", tk.END)  # Clear any previous content
+        text_widget.insert(tk.END, str(data))  # Insert the data as text
+
+    def update_language(self):
+        pass
